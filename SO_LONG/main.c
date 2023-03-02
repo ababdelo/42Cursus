80982aa4366862fe8894bf2cbba25a72d75c0696
Abderrahmane Abdelouafi <ababdelo@e1r10p4.1337.ma>
2023-03-02 15:33:02 +0100

    changing lot of work

--- SO_LONG/main.c
+++ SO_LONG/main.c
@@ -9,6 +9,7 @@ typedef struct t_data
 	void	*mlx;
 	void	*mlx_win;
 	void	*grass_;
+	void	*cllect_;
 	void	*wall_;
 	char 	*buff;
 	int		block_size;
@@ -57,12 +58,13 @@ char	*check_path_and_get_buff(char *path)
 	if (id == -1)
 		print_error("failing open file!\n");
 
-	buff = malloc(sizeof(char) * 1025);
+	buff = malloc(sizeof(char) * 1024);
 	if (!buff)
 		print_error("failed allocating buff !\n");
 	ret = read(id, buff, 1024);
-	if (ret== -1)
+	if (ret == -1)
 		print_error("failing read file!\n");
+	buff[ret] = '\0';
 	close(id);
 	return (buff);
 }
@@ -71,10 +73,13 @@ void	check_texture(s_data *data)
 {
 	int width;
 	int height;
-	data->grass_ = mlx_xpm_file_to_image(data->mlx, "./sources/grass.xpm",  &width, &height);
+	data->grass_ = mlx_xpm_file_to_image(data->mlx, "./sources/Blocks/dirt.xpm",  &width, &height);
 	if(!data->grass_)
 		print_error("couldn't found grass texture !\n");
-	data->wall_ = mlx_xpm_file_to_image(data->mlx, "./sources/wall.xpm",  &width, &height);
+	data->cllect_ = mlx_xpm_file_to_image(data->mlx, "./sources/Collectibles/ender_eye.xpm",  &width, &height);
+	if(!data->cllect_)
+		print_error("couldn't found Collectible texture !\n");
+	data->wall_ = mlx_xpm_file_to_image(data->mlx, "./sources/Blocks/stone_wall.xpm",  &width, &height);
 	if(!data->wall_)
 		print_error("couldn't found wall texture !\n");
 	return ;
@@ -100,7 +105,12 @@ void print2win(s_data *data)
 			mlx_put_image_to_window(data->mlx,data->mlx_win,data->wall_,x,y);
 		else if (data->buff[index] == '0')
 			mlx_put_image_to_window(data->mlx,data->mlx_win,data->grass_,x,y);
+		else if (data->buff[index] == 'C')
+		{
+			mlx_put_image_to_window(data->mlx,data->mlx_win,data->grass_,x,y);
+			mlx_put_image_to_window(data->mlx,data->mlx_win,data->cllect_,x+7,y+7);
 			// mlx_pixel_put(data->mlx, data->mlx_win, x, y, 6819277);
+		}
 		if (data->buff[index] != '\n')
 			x += data->block_size;
 	}
@@ -117,31 +127,51 @@ void	get_map_size(s_data *data)
 	while(data->buff[++index])
 		if(data->buff[index] == '\n')
 			data->width += 1;
-	if(data->buff[index] != '\n')
+	if(data->buff[index] == '\0')
 		data->width += 1;
 }
 
-void	check_map(s_data *data)
+void	check_lines_map_length(s_data *data)
 {
 	int	index;
 	int count;
 	int count_2;
+
 	count = ft_strlen(data->buff,'\n');
 	count_2 = 0;
-	index = -1;
-	while (data->buff[++index])
+	index = 0;
+	while (data->buff[index])
 	{
 		if(data->buff[index] != '\n')
 			count_2++;
 		else
 		{
 			if (count != count_2)
-				print_error("error\n");
+				print_error("lines length doesn't matches !\n");
 			count_2 = 0;
 		}
+		index++;
 	}
 	if (count != count_2)
-		print_error("error\n");
+		print_error("lines length doesn't matches !\n");
+}
+
+void	check_map_border(s_data *data)
+{
+	int	index;
+
+	index = -1;
+	while (data->buff[++index] && data->buff[index] != '\n')
+		if (data->buff[index] != '1')
+			print_error("invalid top map's border\n");
+	index = -1;
+	while(data->buff[++index])
+		if (data->buff[index] == '\n' && index > 0)
+			if (data->buff[index - 1] != '1' || (data->buff[index + 1] != '1' && data->buff[index + 1] != '\0'))
+				print_error("invalid center map's border\n");
+	while(data->buff[--index] != '\n' && index > 0)
+		if (data->buff[index] != '1')
+			print_error("invalid bottom map's border\n");
 }
 
 int main(int argc, char **argv)
@@ -150,16 +180,19 @@ int main(int argc, char **argv)
 	data.height = 0;
 	data.width = 0;
 	data.block_size = 50;
-	if (argc != 2)
+	if (argc == 2)
+	{
+		check_map_extention(argv[1]);
+		data.buff = check_path_and_get_buff(argv[1]);
+		check_lines_map_length(&data);
+		check_map_border(&data);
+		get_map_size(&data);
+		data.mlx =	mlx_init();
+		data.mlx_win = mlx_new_window(data.mlx, data.height * data.block_size, data.width * data.block_size, "Hello");
+		check_texture(&data);
+		print2win(&data);
+		mlx_loop(data.mlx);
+	}
+	else
 		print_error("should be two parameters\n");
-	check_map_extention(argv[1]);
-	data.buff = check_path_and_get_buff(argv[1]);
-	check_map(&data);
-	get_map_size(&data);
-
-	data.mlx =	mlx_init();
-	data.mlx_win = mlx_new_window(data.mlx, data.height * data.block_size, data.width * data.block_size, "Hello");
-	check_texture(&data);
-	print2win(&data);
-	mlx_loop(data.mlx);
 }
