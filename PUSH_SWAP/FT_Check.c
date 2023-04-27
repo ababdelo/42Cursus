c3a7059658720d02bf98f7cd05647c372c4a6570
Abderrahmane Abdelouafi <ababdelo@e1r9p4.1337.ma>
2023-04-27 15:09:59 +0100

    Adding 'PUSH_SWAP' FT_Check.c file

---
+++ PUSH_SWAP/FT_Check.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FT_Check.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababdelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 13:05:10 by ababdelo          #+#    #+#             */
+/*   Updated: 2023/04/27 13:50:34 by ababdelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PUSH_SWAP.h"
+
+void	check_args(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+
+	while (data->av[++i] != NULL)
+	{
+		j = -1;
+		if (data->av[i][0] == '-' || data->av[i][0] == '+')
+			j++;
+		while (data->av[i][++j] != '\0')
+		{
+			if (data->av[i][j] < '0' || data->av[i][j] > '9')
+			{
+				data->err = "NAN";
+				return ;
+			}
+		}
+	}
+}
+
+int	check_repetition(t_data *data)
+{
+	t_node *head;
+	t_node *head2;
+	int	cntr;
+	
+	head = data->stack_a;
+	while(head != NULL)
+	{
+		cntr = 0;
+		head2 = data->stack_a;
+		while(head2 != NULL)
+		{
+			if(head->value == head2->value)
+				cntr++;
+			head2 = head2->next;
+		}
+		if(cntr > 1)
+			return(1);
+		head = head->next;
+	}
+	return(0);
+}
